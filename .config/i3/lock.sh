4437ce3731fd218a59afe73db1b004d91026585a
Sirquini <sirquini2@gmail.com>
2020-05-21 05:00:33 +0200

    Add blur effect to lockscreen background.

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-scrot /tmp/lock_screen.png
-mogrify -scale 10% -scale 1000% /tmp/lock_screen.png
+scrot -o /tmp/lock_screen.png
+mogrify -scale 10% -scale 1000% -blur 0x5 /tmp/lock_screen.png
 i3lock -i /tmp/lock_screen.png
